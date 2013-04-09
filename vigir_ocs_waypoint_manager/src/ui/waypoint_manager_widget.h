fec080743f494a001ab9941cd0016172e87c29a9
Felipe Bacim <fbacim@vt.edu>
2013-04-09 02:32:45 -0400

    - added missing folder

---
+++ vigir_ocs_waypoint_manager/src/ui/waypoint_manager_widget.h
@@ -0,0 +1,58 @@
+#ifndef WaypointManagerWidget_H
+#define WaypointManagerWidget_H
+
+
+#include <QMainWindow>
+#include <QWidget>
+#include <QRadioButton>
+#include <QSpinBox>
+#include <QComboBox>
+#include <QStringList>
+
+#include <QPainter>
+#include <QtGui>
+
+#include <flor_ocs_msgs/OCSWaypointUpdate.h>
+#include <flor_ocs_msgs/OCSWaypointRemove.h>
+#include <geometry_msgs/Pose.h>
+#include <nav_msgs/Path.h>
+#include <ros/ros.h>
+
+namespace rviz
+{
+class RenderPanel;
+class VisualizationManager;
+}
+
+namespace Ui {
+class WaypointManagerWidget;
+}
+
+class WaypointManagerWidget : public QWidget
+{
+    Q_OBJECT
+    
+public:
+    explicit WaypointManagerWidget(QWidget *parent = 0);
+    ~WaypointManagerWidget();
+
+    void processWaypointList(const nav_msgs::Path::ConstPtr& msg);
+    void removeWaypoint(int id);
+
+public Q_SLOTS:
+    void editSlot(int,int);
+    void confirmNavigation();
+
+private:
+    Ui::WaypointManagerWidget* ui;
+
+    ros::NodeHandle nh_;
+    ros::Subscriber waypoint_list_sub_;
+    ros::Publisher waypoint_remove_pub_;
+    ros::Publisher confirm_navigation_pub_;
+
+    rviz::VisualizationManager* manager_;
+    rviz::RenderPanel* render_panel_;
+};
+
+#endif // WaypointManagerWidget_H
