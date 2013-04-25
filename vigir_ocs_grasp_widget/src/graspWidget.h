2e575b30fb46c64d00ed6d2733020a99f86c82d5
Benjamin Waxler <benjw90@vt.edu>
2013-04-24 20:00:12 -0400

    The graspWidget is in mostly working condition.

--- vigir_ocs_grasp_widget/src/graspWidget.h
+++ vigir_ocs_grasp_widget/src/graspWidget.h
@@ -1,7 +1,31 @@
 #ifndef GRASPWIDGET_H
 #define GRASPWIDGET_H
 
+#include <QMainWindow>
 #include <QWidget>
+#include <QRadioButton>
+#include <QSpinBox>
+#include <QComboBox>
+#include <QStringList>
+
+#include <QPainter>
+#include <QtGui>
+
+#include <flor_ocs_msgs/OCSTemplateList.h>
+#include <flor_ocs_msgs/OCSTemplateRemove.h>
+#include <flor_grasp_msgs/GraspState.h>
+#include <flor_grasp_msgs/GraspSelection.h>
+#include <flor_grasp_msgs/TemplateSelection.h>
+#include <geometry_msgs/Pose.h>
+#include <ros/ros.h>
+
+namespace rviz
+{
+class Display;
+class RenderPanel;
+class VisualizationManager;
+class FrameManager;
+}
 
 namespace Ui {
 class graspWidget;
@@ -15,8 +39,69 @@ public:
     explicit graspWidget(QWidget *parent = 0);
     ~graspWidget();
     
+public Q_SLOTS:
+    void on_userSlider_sliderReleased();
+    void on_templateBox_activated(const QString &arg1);
+    void on_performButton_clicked();
+    void on_templateButton_clicked();
+    void on_releaseButton_clicked();
+    void on_manualRadio_clicked();
+    void on_templateRadio_clicked();
+
 private:
+    void setProgressLevel(uint8_t level);
+    void sendManualMsg(uint8_t level);
+    void initTemplateMode();
+    void initTemplateIdMap();
+    std::vector< std::vector<QString> > readTextDBFile(QString path);
+    void initGraspDB();
+
     Ui::graspWidget *ui;
+
+    flor_ocs_msgs::OCSTemplateList lastList;
+    void graspStateRecieved (const flor_grasp_msgs::GraspState::ConstPtr& graspState);
+    void graspSelectedRecieved (const flor_grasp_msgs::GraspSelection::ConstPtr& graspMsg);
+    void processTemplateList( const flor_ocs_msgs::OCSTemplateList::ConstPtr& list);
+    void templateMatchFeedback (const flor_grasp_msgs::TemplateSelection::ConstPtr& feedback);
+
+    QString template_dir_path_;
+    QString grasp_db_path_;
+    QString template_id_db_path_;
+
+    std::map<unsigned char,std::string> template_id_map_;
+    typedef struct
+    {
+        unsigned short grasp_id;
+        unsigned char template_type;
+        std::string template_name;
+        std::string hand;
+        std::string initial_grasp_type;
+        float finger_joints[12];
+        geometry_msgs::Pose final_pose;
+        geometry_msgs::Pose pre_grasp_pose;
+    } GraspDBItem;
+    std::vector<GraspDBItem> grasp_db_;
+    ros::NodeHandle nh_;
+    ros::Subscriber grasp_state_sub_;
+    ros::Subscriber grasp_selected_sub_;
+    ros::Subscriber template_select_sub;
+    ros::Subscriber template_list_sub_;
+    ros::Subscriber template_match_feedback_sub_;
+    ros::Publisher grasp_selected_pub_;
+    ros::Publisher template_remove_pub_;
+    ros::Publisher grasp_selected_state_pub_;
+    ros::Publisher template_match_request_pub_;
+    ros::Publisher grasp_request_pub_;
+
+    rviz::VisualizationManager* manager_;
+    rviz::RenderPanel* render_panel_;
+    rviz::Display* robot_model_;
+
+    geometry_msgs::Pose feedbackPose;
+    bool templateMatchDone;
+    std::string hand;
+//    uint8_t currentGraspState;
+    uint8_t currentGraspMode;
 };
 
 #endif // GRASPWIDGET_H
--- vigir_ocs_grasp_widget/src/main.cpp
+++ vigir_ocs_grasp_widget/src/main.cpp
@@ -3,6 +3,11 @@
 
 int main(int argc, char *argv[])
 {
+    if( !ros::isInitialized() )
+    {
+      ros::init( argc, argv, "grasp_widget", ros::init_options::AnonymousName );
+    }
+
     QApplication a(argc, argv);
     graspWidget w;
     w.show();
