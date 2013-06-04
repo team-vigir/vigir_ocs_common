e806b4b50b0c5a1c7b0158bf674db06848ac1523
vigir@gandolf <vigir@gandolf>
2013-06-04 15:55:41 -0400

    - few modifications for wmctrl

--- vigir_ocs_main_3d_view/src/main.cpp
+++ vigir_ocs_main_3d_view/src/main.cpp
@@ -4,14 +4,28 @@
 
 int main(int argc, char *argv[])
 {
-  if( !ros::isInitialized() )
-  {
-    ros::init( argc, argv, "main_3d_view", ros::init_options::AnonymousName );
-  }
+    if( !ros::isInitialized() )
+    {
+        ros::init( argc, argv, "main_3d_view", ros::init_options::AnonymousName );
+    }
 
-  QApplication a(argc, argv);
-  Main3DViewWidget w;
-  w.show();
-  
-  return a.exec();
+    QApplication a(argc, argv);
+    Main3DViewWidget w;
+    if(argc >= 5)
+    {
+        try
+        {
+            int x = atoi(argv[1]);
+            int y = atoi(argv[2]);
+            int width = atoi(argv[3]);
+            int height = atoi(argv[4]);
+            w.setGeometry(x,y,width,height);
+        }
+        catch(...)
+        {
+        }
+    }
+    w.show();
+
+    return a.exec();
 }
