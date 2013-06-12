c0d15ef64f492b86ad28484fabebf066e71c663f
vigir@gandolf <vigir@gandolf>
2013-06-11 20:13:25 -0400

    - removed joystick widget code that caused map to crash

--- vigir_ocs_map_view/src/ui/map_view_widget.cpp
+++ vigir_ocs_map_view/src/ui/map_view_widget.cpp
@@ -7,7 +7,6 @@
 QRadioButton* joystick;
 QRadioButton* wayPoints;
 QPushButton* insertWaypoints;
-QWidget* joystickWidget;
 
 MapViewWidget::MapViewWidget(QWidget *parent) :
     QWidget(parent),
@@ -18,9 +17,6 @@ MapViewWidget::MapViewWidget(QWidget *parent) :
     wayPoints = this->findChild<QRadioButton *>("waypoint");
 
     insertWaypoints = this->findChild<QPushButton *>("insert_waypoint");
-    joystickWidget= this->findChild<Joystick *>("joystick_widget");
-  //  joystickWidget->setSizePolicy(QSizePolicy::Maximum,QSizePolicy::Maximum);
-    //joystickWidget->setSize(250, 150);
     insertWaypoints->hide();
     connect(joystick, SIGNAL(toggled(bool)), this, SLOT(hideWaypointButton()));
     connect(wayPoints, SIGNAL(toggled(bool)), this, SLOT(hideJoystick()));
@@ -36,12 +32,10 @@ MapViewWidget::~MapViewWidget()
 void MapViewWidget::hideWaypointButton()
 {
     insertWaypoints->hide();
-    joystickWidget->show();
 }
 
 void MapViewWidget::hideJoystick()
 {
-    joystickWidget->hide();
     insertWaypoints->show();
 }
 
