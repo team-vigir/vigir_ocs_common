bf7d2f7303c22a4a7f3663be7bc8175ce2774ef3
David Conner@sam <conner@torcrobotics.com>
2013-06-12 21:20:50 -0400

    added color stats lights for a heartbeat on the topics listed on the widget.

--- vigir_ocs_bandwidth_widget/src/ui/bandwidth_widget.cpp
+++ vigir_ocs_bandwidth_widget/src/ui/bandwidth_widget.cpp
@@ -15,15 +15,7 @@ BandwidthWidget::BandwidthWidget(QWidget *parent) :
     , ui(new Ui::BandwidthWidget)
 {
     ui->setupUi(this);
-    /*
-    ui->tableWidget->setRowCount(1);
-    total_bytes_read_item = new QTableWidgetItem(QString::number(-1));
-    total_bytes_sent_item = new QTableWidgetItem(QString::number(-1));
-    QTableWidgetItem* name = new QTableWidgetItem(QString::fromStdString("TOTAL"));
-
-    ui->tableWidget->setItem(0,0,name);
-    ui->tableWidget->setItem(0,1,total_bytes_read_item);
-    ui->tableWidget->setItem(0,2,total_bytes_sent_item);*/
+    bytes_remaining_initialized = false;
 
 
     // subscribe to the topic to monitor bandwidth usage
@@ -31,11 +23,11 @@ BandwidthWidget::BandwidthWidget(QWidget *parent) :
     // subscribe to the topic to load all waypoints
     vrc_data_sub_ = nh_.subscribe<flor_ocs_msgs::VRCdata>( "/vrc_data", 5, &BandwidthWidget::processVRCData, this );
     timer.start(33, this);
-    foobar = 0;
-
+    topic_heartbeat_sub_ = nh_.subscribe<std_msgs::String>("/flor_ocs_bandwidth/heartbeat",1, &BandwidthWidget::heartbeatRecieved, this);
     ui->tableWidget->setColumnWidth(0,  155);
     ui->tableWidget->setColumnWidth(1,  170);
     ui->tableWidget->setColumnWidth(2,  170);
+    ui->tableWidget->setColumnWidth(3,  80);
 }
 
 BandwidthWidget::~BandwidthWidget()
@@ -43,11 +35,55 @@ BandwidthWidget::~BandwidthWidget()
     delete ui;
 }
 
+void BandwidthWidget::heartbeatRecieved(const std_msgs::String::ConstPtr& msg)
+{
+    int timerNum = 0;
+    for(;timerNum < topicObjectsList.size();timerNum++)
+    {
+        if(ui->tableWidget->item(timerNum,0)->text().toStdString() == msg->data)
+        {
+            topicObjectsList[timerNum]->alarmsSinceReturn = 0;
+            QTableWidgetItem* block = new QTableWidgetItem();
+            block->setBackgroundColor(Qt::green);
+            ui->tableWidget->setItem(timerNum,3,block);
+            topicObjectsList[timerNum]->timer.start(1000,this);
+        }
+    }
+}
+
 void BandwidthWidget::timerEvent(QTimerEvent *event)
 {
     //Spin at beginning of Qt timer callback, so current ROS time is retrieved
-    ros::spinOnce();
+    if(event->timerId() == timer.timerId())
+        ros::spinOnce();
+    else
+    {
+        int timerNum = 0;
+        for(; timerNum< topicObjectsList.size();timerNum++)
+        {
+            if(topicObjectsList[timerNum]->timer.timerId() == event->timerId())
+            {
+                topicObjectsList[timerNum]->timer.start(1000,this);
+                break;
+            }
+        }
+        topicObjectsList[timerNum]->alarmsSinceReturn = topicObjectsList[timerNum]->alarmsSinceReturn + 1;
+        if(topicObjectsList[timerNum]->alarmsSinceReturn > 4)
+        {
+            QTableWidgetItem* block = new QTableWidgetItem();
+            block->setBackgroundColor(Qt::red);
+            ui->tableWidget->setItem(timerNum,3,block);
+        }
+        else if (topicObjectsList[timerNum]->alarmsSinceReturn >2)
+        {
+            QTableWidgetItem* block = new QTableWidgetItem();
+            block->setBackgroundColor(Qt::yellow);
+            ui->tableWidget->setItem(timerNum,3,block);
+        }
+    }
 }
+
+
 
 void BandwidthWidget::processBandwidthMessage(const flor_ocs_msgs::OCSBandwidth::ConstPtr& msg)
 {
@@ -71,7 +107,7 @@ void BandwidthWidget::processBandwidthMessage(const flor_ocs_msgs::OCSBandwidth:
 
         //std::cout << "Adding new item.... rcv_index = " <<rcv_index<<" node bandwith info " << node_bandwidth_info_.size() << " name="<< msg->node_name <<std::endl;
 
-        ui->tableWidget->setRowCount( node_bandwidth_info_.size()+1 );
+        ui->tableWidget->setRowCount( node_bandwidth_info_.size());
         // create new items
         item = new QTableWidgetItem(QString(node_bandwidth_info_[rcv_index].node_name.c_str()));
         ui->tableWidget->setItem(rcv_index,0,item);
@@ -79,6 +115,15 @@ void BandwidthWidget::processBandwidthMessage(const flor_ocs_msgs::OCSBandwidth:
         ui->tableWidget->setItem(rcv_index,1,item);
         item = new QTableWidgetItem(QString::number(node_bandwidth_info_[rcv_index].total_bytes_sent));
         ui->tableWidget->setItem(rcv_index,2,item);
+        item = new QTableWidgetItem();
+        item->setBackgroundColor(Qt::green);
+        ui->tableWidget->setItem(rcv_index,3,item);
+
+        topicObject* objTopic = new topicObject;
+        objTopic->timer.start(1000,this);
+        objTopic->alarmsSinceReturn = 0;
+
+        topicObjectsList.push_back(objTopic);
 
     }
     else // if it already exists
@@ -99,18 +144,27 @@ void BandwidthWidget::processBandwidthMessage(const flor_ocs_msgs::OCSBandwidth:
         total_read += node_bandwidth_info_[i].total_bytes_read;
         total_sent += node_bandwidth_info_[i].total_bytes_sent;
     }
-    total_bytes_sent_item = new QTableWidgetItem(QString::number(total_sent));
-    total_bytes_read_item = new QTableWidgetItem(QString::number(total_read));
-    QTableWidgetItem* name = new QTableWidgetItem(QString::fromStdString("TOTAL"));
-    ui->tableWidget->setItem(node_bandwidth_info_.size(),0,name);
-    ui->tableWidget->setItem(node_bandwidth_info_.size(),1,total_bytes_read_item);
-    ui->tableWidget->setItem(node_bandwidth_info_.size(),2,total_bytes_sent_item);
+    ui->downTotalLabel->setText(QString::number(total_read));
+    ui->upTotalLabel->setText(QString::number(total_sent));
+//    total_bytes_sent_item = new QTableWidgetItem(QString::number(total_sent));
+//    total_bytes_read_item = new QTableWidgetItem(QString::number(total_read));
+//    QTableWidgetItem* name = new QTableWidgetItem(QString::fromStdString("TOTAL"));
+//    ui->tableWidget->setItem(node_bandwidth_info_.size(),0,name);
+//    ui->tableWidget->setItem(node_bandwidth_info_.size(),1,total_bytes_read_item);
+//    ui->tableWidget->setItem(node_bandwidth_info_.size(),2,total_bytes_sent_item);
+
+    //double temp = (double)(node_bandwidth_info_[rcv_index].total_bytes_read)/(double)total_read;
+    //item = new QTableWidgetItem(QString::number(temp));
+    //ui->tableWidget->setItem(rcv_index,4,item);
+    //item = new QTableWidgetItem(QString::number(node_bandwidth_info_[rcv_index].total_bytes_sent/total_sent));
+    //ui->tableWidget->setItem(rcv_index,3,item);
 
     if(starting_row_count < 2)
     {
         ui->tableWidget->setColumnWidth(0,  155);
         ui->tableWidget->setColumnWidth(1,  170);
         ui->tableWidget->setColumnWidth(2,  170);
+        ui->tableWidget->setColumnWidth(3,  80);
     }
 }
 
@@ -141,12 +195,21 @@ QString timeFromMsg(const ros::Time& stamp)
 
 void BandwidthWidget::processVRCData(const flor_ocs_msgs::VRCdata::ConstPtr& msg)
 {
-    ui->sim_time_elapsed->setText(timeFromMsg(msg->sim_time_elapsed));
     ui->competition_score->setText(QString::number(msg->competition_score));
     ui->falls->setText(QString::number(msg->falls));
     ui->message->setText(QString(msg->message.c_str()));
-    ui->task_type->setText(QString::number(msg->task_type));
-    ui->remaining_download->setText(QString::number(msg->downlink_bytes_remaining));
-    ui->remaining_upload->setText(QString::number(msg->uplink_bytes_remaining));
+    if(!bytes_remaining_initialized && msg->downlink_bytes_remaining < UINT_MAX)
+    {
+        down_max = msg->downlink_bytes_remaining;
+        up_max = msg->downlink_bytes_remaining;
+        bytes_remaining_initialized = true;
+    }
+    else if (bytes_remaining_initialized)
+    {
+        ui->down_remaining_bar->setValue((msg->downlink_bytes_remaining/down_max)*1000);
+        ui->up_remaining_bar->setValue((msg->uplink_bytes_remaining/up_max)*1000);
+    }
+    //ui->remaining_download->setText(QString::number(msg->downlink_bytes_remaining));
+    //ui->remaining_upload->setText(QString::number(msg->uplink_bytes_remaining));
 }
 
