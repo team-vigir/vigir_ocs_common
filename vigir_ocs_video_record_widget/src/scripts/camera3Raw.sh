0db682d718af9ac49ebf53973c3d195d7f8a788e
Stefan Kohlbrecher <kohlbrecher@sim.tu-darmstadt.de>
2013-08-29 11:41:27 +0200

    [vigir_ocs_common] * Remove video widget as it was moved to separate repo (#936)

--- vigir_ocs_video_record_widget/src/scripts/camera3Raw.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd $1
-plugctl -n 1 oPCR[1].bcast_connection=0
-dvgrab -format dv1 -guid 08004601043d97ca - | tee >(playdv --disable-audio --no-mmap -d 1) >(dvgrab -format dv1 -stdin $2_)
--- vigir_ocs_video_record_widget/src/scripts/camera4Raw.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd $1
-plugctl -n 0oPCR[2].bcast_connection=0
-dvgrab -format dv1 -guid 08004601043d97d7 - | tee >(playdv --disable-audio --no-mmap -d 1) >(dvgrab -format dv1 -stdin $2_)
