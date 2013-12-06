e5e4b928558de4909cb500573b6b8ed6651cc81f
vigir@gandalf <vigir@gandalf>
2013-12-05 21:17:18 -0500

    - fixed octomap request issues

--- vigir_ocs_base_3d_view/src/base_3d_view.cpp
+++ vigir_ocs_base_3d_view/src/base_3d_view.cpp
@@ -150,9 +150,9 @@ Base3DView::Base3DView( rviz::VisualizationManager* context, std::string base_fr
         laser_scan_->subProp( "Selectable" )->setValue( false );
 
         // Create a MarkerArray display.
-        octomap_ = manager_->createDisplay( "rviz/OctomapDisplayCustom", "Octomap", false );
+        octomap_ = manager_->createDisplay( "octomap_rviz_plugin/OccupancyGrid", "Octomap", true );
         ROS_ASSERT( octomap_ != NULL );
-        octomap_->subProp( "Marker Topic" )->setValue( "/flor/ocs/worldmodel/octomap_result" );
+        octomap_->subProp( "Octomap Topic" )->setValue( "/flor/ocs/worldmodel/octomap_result" );
 
         // Create a point cloud display.
         stereo_point_cloud_viewer_ = manager_->createDisplay( "rviz/PointCloud2", "Point Cloud", false );
