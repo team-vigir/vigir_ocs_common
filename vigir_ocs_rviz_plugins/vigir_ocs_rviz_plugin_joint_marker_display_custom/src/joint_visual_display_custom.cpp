03d0f65429f41709e20d8c678e6cffd48bde7528
vigir <vigir@bilbo>
2014-09-22 13:34:03 -0400

    adding files for joint markers

---
+++ vigir_ocs_rviz_plugins/vigir_ocs_rviz_plugin_joint_marker_display_custom/src/joint_visual_display_custom.cpp
@@ -0,0 +1,272 @@
+
+#include "joint_visual_display_custom.h"
+
+
+/*
+  Added ability to change colors. Scale/position/color are no longer based on effort
+
+  */
+namespace rviz
+{
+
+    EffortVisualCustom::EffortVisualCustom( Ogre::SceneManager* scene_manager, Ogre::SceneNode* parent_node, boost::shared_ptr<urdf::Model> urdf_model )
+    {
+        scene_manager_ = scene_manager;
+
+        // Ogre::SceneNode s form a tree, with each node storing the
+        // transform (position and orientation) of itself relative to its
+        // parent.  Ogre does the math of combining those transforms when it
+        // is time to render.
+        //
+        // Here we create a node to store the pose of the Effort's header frame
+        // relative to the RViz fixed frame.
+        frame_node_ = parent_node->createChildSceneNode();
+
+            //
+            urdf_model_ = urdf_model;
+
+        // We create the arrow object within the frame node so that we can
+        // set its position and direction relative to its header frame.
+        for (std::map<std::string, boost::shared_ptr<urdf::Joint> >::iterator it = urdf_model_->joints_.begin(); it != urdf_model_->joints_.end(); it ++ ) {
+            if ( it->second->type == urdf::Joint::REVOLUTE ) {
+                    std::string joint_name = it->first;
+                    effort_enabled_[joint_name] = true;
+                }
+            }
+        //default to green
+        color_ = new Ogre::ColourValue(0,1.0,0,.9f);
+
+        marker_scale_ = .08f;
+
+    }
+
+    EffortVisualCustom::~EffortVisualCustom()
+    {
+        // Delete the arrow to make it disappear.
+        for (std::map<std::string, rviz::BillboardLine*>::iterator it = effort_circle_.begin(); it != effort_circle_.end(); it ++ ) {
+                delete (it->second);
+            }
+        for (std::map<std::string, rviz::Arrow*>::iterator it = effort_arrow_.begin(); it != effort_arrow_.end(); it ++ ) {
+                delete (it->second);
+            }        
+
+        // Destroy the frame node since we don't need it anymore.
+        scene_manager_->destroySceneNode( frame_node_ );
+
+        delete(color_);
+    }
+
+
+    void EffortVisualCustom::getRainbowColor(float value, Ogre::ColourValue& color)
+    {
+    value = std::min(value, 1.0f);
+    value = std::max(value, 0.0f);
+
+    float h = value * 5.0f + 1.0f;
+    int i = floor(h);
+    float f = h - i;
+    if ( !(i&1) ) f = 1 - f; // if i is even
+    float n = 1 - f;
+
+    if      (i <= 1) color[0] = 0, color[1] = n, color[2] = 1;
+    else if (i == 2) color[0] = 0, color[1] = 1, color[2] = n;
+    else if (i == 3) color[0] = n, color[1] = 1, color[2] = 0;
+    else if (i == 4) color[0] = 1, color[1] = n, color[2] = 0;
+    else if (i >= 5) color[0] = n, color[1] = 0, color[2] = 1;
+    }
+
+    void EffortVisualCustom::setMessage( const sensor_msgs::JointStateConstPtr& msg )
+    {
+    // for all joints...
+    int joint_num = msg->name.size();
+    for (int i = 0; i < joint_num; i++ )
+    {
+        std::string joint_name = msg->name[i];
+        //double effort = msg->effort[i];
+        const urdf::Joint* joint = urdf_model_->getJoint(joint_name).get();
+        int joint_type = joint->type;
+        if ( joint_type == urdf::Joint::REVOLUTE )
+        {
+                // enable or disable draw
+                if ( effort_circle_.find(joint_name) != effort_circle_.end() &&
+                     !effort_enabled_[joint_name] ) // enable->disable
+                    {
+                        delete(effort_circle_[joint_name]);
+                        delete(effort_arrow_[joint_name]);
+                        effort_circle_.erase(joint_name);
+                        effort_arrow_.erase(joint_name);
+                    }
+                if ( effort_circle_.find(joint_name) == effort_circle_.end() &&
+                     effort_enabled_[joint_name] ) // disable -> enable
+                    {
+                        effort_circle_[joint_name] = new rviz::BillboardLine( scene_manager_, frame_node_ );
+                       // setRenderOrder(effort_circle_[joint_name]->getSceneNode());
+                        effort_arrow_[joint_name] = new rviz::Arrow( scene_manager_, frame_node_ );
+                        //setRenderOrder(effort_arrow_[joint_name]->getSceneNode());
+                    }
+
+                if ( ! effort_enabled_[joint_name] ) continue;
+
+        //tf::Transform offset = poseFromJoint(joint);
+        boost::shared_ptr<urdf::JointLimits> limit = joint->limits;
+
+                effort_arrow_[joint_name]->set(0, width_*2, width_*2*1.0, width_*2*2.0);
+                //arrow direction
+                if ( arrow_directions_.find(joint_name) != arrow_directions_.end() )
+                {                                                                                       //-1 or 1 for direction
+                    effort_arrow_[joint_name]->setDirection(orientation_[joint_name] * Ogre::Vector3(arrow_directions_[joint_name],0,0));
+                }
+                effort_arrow_[joint_name]->setPosition(orientation_[joint_name] * Ogre::Vector3(0, 0.05+marker_scale_*scale_*0.5, 0) + position_[joint_name]);
+
+                effort_circle_[joint_name]->clear();
+                effort_circle_[joint_name]->setLineWidth(width_);
+                for (int i = 0; i < 30; i++)
+                {
+                    Ogre::Vector3 point = Ogre::Vector3((0.05+marker_scale_*scale_*0.5)*sin(i*2*M_PI/32), (0.05+marker_scale_*scale_*0.5)*cos(i*2*M_PI/32), 0);
+//                    if ( effort < 0 ) point.x = -point.x;
+                    effort_circle_[joint_name]->addPoint(orientation_[joint_name] * point + position_[joint_name]);
+                }
+               // Ogre::ColourValue color;
+                //getRainbowColor(effort_value, color);
+//                if(color_ != NULL)
+//                {
+//                    effort_arrow_[joint_name]->setColor(color_->r, color_->g, color_->b, color_->a);
+//                    effort_circle_[joint_name]->setColor(color_->r, color_->g, color_->b, color_->a);
+//                }
+//                else
+//                {
+//                    effort_arrow_[joint_name]->setColor(0,1, 0, .9f);
+//                    effort_circle_[joint_name]->setColor(0,1, 0, .9f);
+//                }
+
+
+//                Ogre::Quaternion rotation = effort_circle_[joint_name]->getOrientation();
+//               // ROS_ERROR("before %f %f %f %f",rotation.w,rotation.x,rotation.y,rotation.z);
+//                Ogre::Quaternion* rApplied = new Ogre::Quaternion(1.0f,0.5f,0,0);
+//                rotation = rotation * *rApplied;
+//               // ROS_ERROR("after %f %f %f %f",rotation.w,rotation.x,rotation.y,rotation.z);
+//                effort_circle_[joint_name]->setOrientation(orientation_[joint_name] * rotation);
+//                delete(rApplied);
+
+            }
+        }
+    }
+
+    void EffortVisualCustom::setArrowDirection(std::string jointName,int direction)
+    {
+        arrow_directions_[jointName] = direction;
+    }
+
+    void EffortVisualCustom::setRenderOrder(Ogre::SceneNode* sceneNode)
+    {
+        for(int i =0;i<sceneNode->numAttachedObjects();i++)
+        {
+
+            Ogre::MovableObject* obj =  sceneNode->getAttachedObject(i);
+            obj->setRenderQueueGroup(Ogre::RENDER_QUEUE_MAIN - 1);
+            //ROS_ERROR("set line");
+        }
+        for(int i =0;i<sceneNode->numChildren();i++)
+        {
+            setRenderOrder((Ogre::SceneNode*)sceneNode->getChild(i));
+        }
+    }
+
+    void EffortVisualCustom::setFrameEnabled( const std::string joint_name, const bool e )
+    {
+        effort_enabled_[joint_name] = e;
+    }
+
+    // Position and orientation are passed through to the SceneNode.
+    void EffortVisualCustom::setFramePosition( const Ogre::Vector3& position )
+    {
+    frame_node_->setPosition( position );
+    }
+
+    void EffortVisualCustom::setFrameOrientation( const Ogre::Quaternion& orientation )
+    {
+    frame_node_->setOrientation( orientation );
+    }
+    // Position and orientation are passed through to the SceneNode.
+    void EffortVisualCustom::setFramePosition( const std::string joint_name, const Ogre::Vector3& position )
+    {
+    position_[joint_name] = position;
+    }
+
+    void EffortVisualCustom::setFrameOrientation( const std::string joint_name, const Ogre::Quaternion& orientation )
+    {
+    orientation_[joint_name] = orientation;
+    }   
+
+    void EffortVisualCustom::setWidth( float w )
+    {
+        width_ = w;
+    }
+
+    void EffortVisualCustom::setScale( float s )
+    {
+        scale_ = s;
+    }
+    void EffortVisualCustom::setAlpha(float a)
+    {
+        color_->a = a;
+        for (std::map<std::string, rviz::BillboardLine*>::iterator it=effort_circle_.begin(); it!=effort_circle_.end(); ++it)
+        {
+            rviz::BillboardLine* line = it->second;
+            line->setColor(color_->r,color_->g,color_->b,color_->a);
+        }
+        for (std::map<std::string, rviz::Arrow*>::iterator it=effort_arrow_.begin(); it!=effort_arrow_.end(); ++it)
+        {
+            rviz::Arrow* arrow = it->second;
+            arrow->setColor(color_->r,color_->g,color_->b,color_->a);
+        }
+    }
+    void EffortVisualCustom::setJointAlpha(float a, std::string joint_name)
+    {
+        color_->a = a;
+        if(effort_circle_.count(joint_name)> 0)// && effort_arrow_.count(joint_name)> 0)
+        {
+            //ROS_ERROR("set alpha %s %f", joint_name.c_str(), a);
+            effort_circle_[joint_name]->setColor(color_->r,color_->g,color_->b,color_->a);
+            effort_arrow_[joint_name]->setColor(color_->r,color_->g,color_->b,color_->a);
+        }
+        else
+        {
+            ROS_ERROR("Tried to change non-existing joint alpha\n");
+        }
+    }
+    void EffortVisualCustom::setColor( float r, float g, float b)
+    {
+        color_->r = r;
+        color_->g = g;
+        color_->b = b;
+        for (std::map<std::string, rviz::BillboardLine*>::iterator it=effort_circle_.begin(); it!=effort_circle_.end(); ++it)
+        {
+            rviz::BillboardLine* line = it->second;
+            line->setColor(color_->r,color_->g,color_->b,color_->a);
+        }
+        for (std::map<std::string, rviz::Arrow*>::iterator it=effort_arrow_.begin(); it!=effort_arrow_.end(); ++it)
+        {
+            rviz::Arrow* arrow = it->second;
+            arrow->setColor(color_->r,color_->g,color_->b,color_->a);
+        }
+    }
+    void EffortVisualCustom::setJointColor( float r, float g, float b, std::string joint_name)
+    {
+        color_->r = r;
+        color_->g = g;
+        color_->b = b;
+        if(effort_circle_.count(joint_name)> 0)// && effort_arrow_.count(joint_name)> 0)
+        {
+           //ROS_ERROR("set color %s %f", joint_name.c_str(),color_->g);
+            effort_circle_[joint_name]->setColor(color_->r,color_->g,color_->b,color_->a);
+            effort_arrow_[joint_name]->setColor(color_->r,color_->g,color_->b,color_->a);
+        }
+        else
+        {
+            ROS_ERROR("Tried to change non-existing joint color");
+        }
+    }
+
+} // end namespace rviz
+
---
+++ vigir_ocs_rviz_plugins/vigir_ocs_rviz_plugin_joint_marker_display_custom/src/joint_visual_display_custom.h
@@ -0,0 +1,123 @@
+#ifndef JOINT_VISUAL_DISPLAY_CUSTOM_H
+#define JOINT_VISUAL_DISPLAY_CUSTOM_H
+
+#include <OgreVector3.h>
+#include <OgreSceneNode.h>
+#include <OgreSceneManager.h>
+
+#include <rviz/ogre_helpers/arrow.h>
+#include <rviz/ogre_helpers/billboard_line.h>
+
+#include <ros/ros.h>
+
+#include <urdf/model.h>
+#include <sensor_msgs/JointState.h>
+
+namespace Ogre
+{
+    class Vector3;
+    class Quaternion;
+}
+
+namespace urdf
+{
+    class Model;
+}
+
+namespace rviz
+{
+    class Arrow;
+    class BillboardLine;
+}
+
+namespace rviz
+{
+
+
+// Each instance of EffortVisualCustom represents the visualization of a single
+// sensor_msgs::Effort message.  Currently it just shows an arrow with
+// the direction and magnitude of the acceleration vector, but could
+// easily be expanded to include more of the message data.
+class EffortVisualCustom
+{
+public:
+    // Constructor.  Creates the visual stuff and puts it into the
+    // scene, but in an unconfigured state.
+    EffortVisualCustom( Ogre::SceneManager* scene_manager, Ogre::SceneNode* parent_node, boost::shared_ptr<urdf::Model> urdf_model );
+
+    // Destructor.  Removes the visual stuff from the scene.
+    virtual ~EffortVisualCustom();
+
+    //set arrow direction for specific joints
+    void setArrowDirection(std::string jointName,int direction);
+
+    // set rainbow color
+    void getRainbowColor(float value, Ogre::ColourValue& color);
+    // Configure the visual to show the data in the message.
+    void setMessage( const sensor_msgs::JointStateConstPtr& msg );
+
+    // Set the pose of the coordinate frame the message refers to.
+    // These could be done inside setMessage(), but that would require
+    // calls to FrameManager and error handling inside setMessage(),
+    // which doesn't seem as clean.  This way EffortVisualCustom is only
+    // responsible for visualization.
+    void setFramePosition( const Ogre::Vector3& position );
+    void setFrameOrientation( const Ogre::Quaternion& orientation );
+
+    // set the pose of coordinates frame the each joint refers to.
+    void setFramePosition( const std::string joint_name, const Ogre::Vector3& position );
+    void setFrameOrientation( const std::string joint_name, const Ogre::Quaternion& orientation );
+
+    void setFrameEnabled( const std::string joint_name, const bool e );
+
+    //set individual colors/alpha based on joint name to target specific joints
+    void setJointColor( float r, float g, float b, std::string joint);
+    void setJointAlpha(float a, std::string joint);
+
+    void setAlpha(float a);
+    void setColor( float r, float g, float b);
+    void setWidth( float w );
+    void setScale( float s );
+
+
+
+
+
+private:
+    void setRenderOrder(Ogre::SceneNode* sceneNode);
+
+    //stores directions for all arrows, can be 1 or -1 to indicate direction, can be modified externally
+    std::map<std::string,int> arrow_directions_;
+
+    // The object implementing the effort circle           
+    std::map<std::string, rviz::BillboardLine*> effort_circle_;
+    std::map<std::string, rviz::Arrow*> effort_arrow_;
+    std::map<std::string, bool> effort_enabled_;
+
+    // A SceneNode whose pose is set to match the coordinate frame of
+    // the Effort message header.
+    Ogre::SceneNode* frame_node_;
+
+    // The SceneManager, kept here only so the destructor can ask it to
+    // destroy the ``frame_node_``.
+    Ogre::SceneManager* scene_manager_;
+
+    Ogre::ColourValue* color_;
+    float alpha_;
+
+    std::map<std::string, Ogre::Vector3> position_;
+    std::map<std::string, Ogre::Quaternion> orientation_;
+
+    //used to determine circular size of markers
+    float marker_scale_;
+
+    float width_, scale_;
+
+    // The object for urdf model
+    boost::shared_ptr<urdf::Model> urdf_model_;
+};
+
+} // end namespace rviz
+
+#endif // JOINT_VISUAL_DISPLAY_CUSTOM_H
+
