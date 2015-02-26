4b9a3d6ad18909dc8c51fb15c35e94f90a83807e
Benjamin Waxler <benjw90@vt.edu>
2015-02-26 10:04:44 -0500

    Fixed logging reporting to commander widget.

--- vigir_ocs_logging/src/widget.cpp
+++ vigir_ocs_logging/src/widget.cpp
@@ -9,6 +9,7 @@
 #include <iomanip>
 #include <ctime>
 #include "QSpinBox"
+#include "QLabel"
 
 #define ROBOT_IP "192.168.130.103"
 
@@ -19,12 +20,13 @@ Widget::Widget(QWidget *parent) :
     ros::start();
 
     ui->setupUi(this);
-    ros::NodeHandle nh;
+    ros::NodeHandle nh("~");
+    timer.start(33, this);
     ocs_logging_pub_ = nh.advertise<flor_ocs_msgs::OCSLogging>("/vigir_logging",        1, false);
-    ocs_responce_sub_ = nh.subscribe<std_msgs::String>("/vigir_logging_responce", 10, &Widget::on_responce_recieved, this);
+    ocs_responce_sub_ = nh.subscribe<std_msgs::String>("/vigir_logging_responce", 5, &Widget::on_responce_recieved, this);
     ocs_responce_pub_ = nh.advertise<std_msgs::String>("/vigir_logging_query", 1, false);
 	first = true;
-    timer.start(33, this);
+
 }
 
 Widget::~Widget()
@@ -39,31 +41,44 @@ void Widget::on_responce_recieved(const std_msgs::String::ConstPtr& msg)
     {
         std::cout << "Video Start" << std::endl;
         //change text and color of videoLogging to green
+        ui->videoLogging->setStyleSheet("color: rgb(0,255,0)");
+        ui->videoLogging->setText(QString::fromStdString("Video Running"));
     }
     else if(msg->data == "video_stop")
     {
         //change text and color of videoLogging to red
         std::cout << "Video Start" << std::endl;
+        ui->videoLogging->setStyleSheet("color: rgb(255,0,0)");
+        ui->videoLogging->setText(QString::fromStdString("Video Stopped"));
     }
-    else if(msg->data == "ocs_stop")
+    else if(msg->data == "ocs_start")
     {
         //change text and color of OCSLogging to red
         std::cout << "OCS Start" << std::endl;
+        ui->OCULogging->setStyleSheet("color: rgb(0,255,0)");
+        ui->OCULogging->setText(QString::fromStdString("OCS Running"));
+
     }
     else if(msg->data == "ocs_stop")
     {
         //change text and color of OCSLogging to red
         std::cout << "OCS Stop" << std::endl;
+        ui->OCULogging->setStyleSheet("color: rgb(255,0,0)");
+        ui->OCULogging->setText(QString::fromStdString("OCS Stopped"));
     }
-    else if(msg->data == "onboard_stop")
+    else if(msg->data == "onboard_start")
     {
         //change text and color of onboardLogging to red
         std::cout << "Onboard Start" << std::endl;
+        ui->onboardLogging->setStyleSheet("color: rgb(0,255,0)");
+        ui->onboardLogging->setText(QString::fromStdString("Onboard Running"));
     }
     else if(msg->data == "onboard_stop")
     {
         //change text and color of onboardLogging to red
         std::cout << "Onboard Stop" << std::endl;
+        ui->onboardLogging->setStyleSheet("color: rgb(255,0,0)");
+        ui->onboardLogging->setText(QString::fromStdString("Onboard Stopped"));
     }
 
 }
@@ -81,8 +96,9 @@ void Widget::timerEvent(QTimerEvent *event)
         std::cout << "querying for the state of all children..." << std::endl;        
         std_msgs::String query;
         query.data = "?";
+        first = false;ros::spinOnce();
         ocs_responce_pub_.publish(query);
-        first = false;
+        ros::spinOnce();
     }
 }
 
