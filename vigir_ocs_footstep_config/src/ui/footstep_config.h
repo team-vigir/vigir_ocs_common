ea2356e38dc35f745fb47d5cf95c65c692a352b5
vigir@gimli <vigir@gimli>
2015-04-13 17:48:22 -0400

    added missing package

---
+++ vigir_ocs_footstep_config/src/ui/footstep_config.h
@@ -0,0 +1,43 @@
+#ifndef FOOTSTEO_CONFIGURE_H
+#define FOOTSTEO_CONFIGURE_H
+
+#include <QWidget>
+#include <ros/ros.h>
+#include <ros/package.h>
+
+
+namespace Ui
+{
+    class FootstepConfigure;
+}
+
+class FootstepConfigure : public QWidget
+{
+    Q_OBJECT
+    
+public:
+    explicit FootstepConfigure(QWidget *parent = 0);
+    virtual ~FootstepConfigure();
+
+    double getMaxTime();
+    int getMaxSteps();
+    double getPlanLengthRatio();
+    int getFootstepInteraction();
+    bool patternGenerationEnabled();
+
+public Q_SLOTS:
+    void updateFootstepParamaters();
+    void updateFootstepParamaters(int ignore);
+    void updateFootstepParamaters(double ignore);
+
+Q_SIGNALS:
+    void sendFootstepParamaters(double,int,double,int,bool);
+
+private:
+    Ui::FootstepConfigure *ui;
+    QString icon_path_;
+
+
+};
+
+#endif //FOOTSTEO_CONFIGURE_H
