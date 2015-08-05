39900ed6e00a4ef6312785df74b2cde24fdce9c6
Benjamin Waxler <benjamin.waxler@gmail.com>
2015-08-05 10:19:48 -0700

    moved ocs messages to inside ocs_common repo

---
+++ vigir_ocs_msgs/include/vigir_ocs_msgs/WindowCodes.h
@@ -0,0 +1,53 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2013-2015, Team ViGIR ( TORC Robotics LLC, TU Darmstadt, Virginia Tech, Oregon State University, Cornell University, and Leibniz University Hanover )
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Team ViGIR, TORC Robotics, nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+//@TODO_ADD_AUTHOR_INFO
+// defines for all windows that can be controlled ->
+//   positive n means "show window n"
+//   negative n means "hide window n"
+#define HIDE_ALL_WINDOWS           0
+#define WINDOW_FOOTSTEP_BASIC      1
+#define WINDOW_FOOTSTEP_ADVANCED   2
+#define WINDOW_FOOTSTEP_PARAMETER  3
+#define WINDOW_BDI_PELVIS_POSE     4
+#define WINDOW_GHOST_CONFIG        5
+#define WINDOW_PLANNER_CONFIG      6
+#define WINDOW_JOINT_CONTROL       7
+#define WINDOW_POSITION_MODE       8
+#define WINDOW_CONTROL_MODE        9
+#define WINDOW_GLANCE_HUB         10
+#define WINDOW_STATUS_WINDOW      11
+#define WINDOW_SYSTEM_COMMAND     12
+#define WINDOW_JOYSTICK           13
+#define WINDOW_SYSTEM_LOG         14
+#define WINDOW_JOINT_STATUS       15
