4d0eda786f01448e5707ff19703c47b640ba6a9e
Achim Stein <steinachim@gmx.de>
2015-08-12 19:11:59 +0200

    fix merge error

--- vigir_ocs_base_3d_view/src/footstep_vis_manager.cpp
+++ vigir_ocs_base_3d_view/src/footstep_vis_manager.cpp
@@ -353,7 +353,7 @@ void FootstepVisManager::processGoalPoseFeedback(const vigir_ocs_msgs::OCSFootst
             marker.orientation.x = plan_goal->goal_pose.pose.orientation.x;
             marker.orientation.y = plan_goal->goal_pose.pose.orientation.y;
             marker.orientation.z = plan_goal->goal_pose.pose.orientation.z;
-            marker.mode = flor_ocs_msgs::OCSInteractiveMarkerAdd::WAYPOINT_3DOF;
+            marker.mode = vigir_ocs_msgs::OCSInteractiveMarkerAdd::WAYPOINT_3DOF;
 
 //             interactive_marker_add_pub_.publish(marker);
 
@@ -387,15 +387,13 @@ void FootstepVisManager::processGoalPoseFeedback(const vigir_ocs_msgs::OCSFootst
                 marker.point.x = i ? plan_goal->right_foot.pose.position.x : plan_goal->left_foot.pose.position.x;
                 marker.point.y = i ? plan_goal->right_foot.pose.position.y : plan_goal->left_foot.pose.position.y;
                 marker.point.z = i ? plan_goal->right_foot.pose.position.z : plan_goal->left_foot.pose.position.z;
-<<<<<<< HEAD
-                marker.mode = vigir_ocs_msgs::OCSInteractiveMarkerAdd::OBJECT_6DOF;
-=======
+
                 marker.orientation.w = plan_goal->right_foot.pose.orientation.w;
                 marker.orientation.x = plan_goal->right_foot.pose.orientation.x;
                 marker.orientation.y = plan_goal->right_foot.pose.orientation.y;
                 marker.orientation.z = plan_goal->right_foot.pose.orientation.z;
-                marker.mode = flor_ocs_msgs::OCSInteractiveMarkerAdd::OBJECT_6DOF;
->>>>>>> 2a440ae... fixed orientation issue with the footstep goal marker
+                marker.mode = vigir_ocs_msgs::OCSInteractiveMarkerAdd::OBJECT_6DOF;
+
                 interactive_marker_add_pub_.publish(marker);
 
                 rviz::Display* im = manager_->createDisplay( "rviz/InteractiveMarkers", (std::string("Interactive marker - Footstep  Goal ")+(i ? "Right" : "Left")).c_str(), false );
@@ -474,9 +472,7 @@ void FootstepVisManager::updateInteractiveMarkers()
                 marker.point.x = (footstep_list_.pose[i].pose.position.x+footstep_list_.pose[i-1].pose.position.x)/2.0;
                 marker.point.y = (footstep_list_.pose[i].pose.position.y+footstep_list_.pose[i-1].pose.position.y)/2.0;
                 marker.point.z = (footstep_list_.pose[i].pose.position.z+footstep_list_.pose[i-1].pose.position.z)/2.0;
-<<<<<<< HEAD
-                marker.mode = vigir_ocs_msgs::OCSInteractiveMarkerAdd::WAYPOINT_3DOF;
-=======
+
                 Ogre::Quaternion q1(footstep_list_.pose[i-1].pose.orientation.w,footstep_list_.pose[i-1].pose.orientation.x,footstep_list_.pose[i-1].pose.orientation.y,footstep_list_.pose[i-1].pose.orientation.z);
                 Ogre::Quaternion q2(footstep_list_.pose[i].pose.orientation.w,footstep_list_.pose[i].pose.orientation.x,footstep_list_.pose[i].pose.orientation.y,footstep_list_.pose[i].pose.orientation.z);
                 Ogre::Quaternion qr = Ogre::Quaternion::Slerp(0.5,q1,q2);
@@ -484,8 +480,8 @@ void FootstepVisManager::updateInteractiveMarkers()
                 marker.orientation.x = qr.x;
                 marker.orientation.y = qr.y;
                 marker.orientation.z = qr.z;
-                marker.mode = flor_ocs_msgs::OCSInteractiveMarkerAdd::WAYPOINT_3DOF;
->>>>>>> 2a440ae... fixed orientation issue with the footstep goal marker
+                marker.mode = vigir_ocs_msgs::OCSInteractiveMarkerAdd::WAYPOINT_3DOF;
+
                 interactive_marker_add_pub_.publish(marker);
 
                 rviz::Display* im = manager_->createDisplay( "rviz/InteractiveMarkers", (std::string("Interactive marker - Step Plan ")+boost::lexical_cast<std::string>(i)).c_str(), true );
@@ -539,15 +535,13 @@ void FootstepVisManager::updateInteractiveMarkers()
             marker.point.x = footstep_list_.pose[i].pose.position.x;
             marker.point.y = footstep_list_.pose[i].pose.position.y;
             marker.point.z = footstep_list_.pose[i].pose.position.z;
-<<<<<<< HEAD
-            marker.mode = vigir_ocs_msgs::OCSInteractiveMarkerAdd::OBJECT_6DOF;
-=======
+
             marker.orientation.w = footstep_list_.pose[i].pose.orientation.w;
             marker.orientation.x = footstep_list_.pose[i].pose.orientation.x;
             marker.orientation.y = footstep_list_.pose[i].pose.orientation.y;
             marker.orientation.z = footstep_list_.pose[i].pose.orientation.z;
-            marker.mode = flor_ocs_msgs::OCSInteractiveMarkerAdd::OBJECT_6DOF;
->>>>>>> 2a440ae... fixed orientation issue with the footstep goal marker
+            marker.mode = vigir_ocs_msgs::OCSInteractiveMarkerAdd::OBJECT_6DOF;
+
             interactive_marker_add_pub_.publish(marker);
 
             rviz::Display* im = manager_->createDisplay( "rviz/InteractiveMarkers", (std::string("Interactive marker - Footstep ")+boost::lexical_cast<std::string>(i)).c_str(), false );
